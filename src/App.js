45591fdac2be0d4dde1773d2235a959dec62a23f
Apurva <apurvaanand471@gmail.com>
2022-05-03 11:55:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import NewsFeed from './newsfeed';
-import './App.css';
-import { useState } from 'react';
-import Navbar from './navbar';
+import NewsFeed from "./newsfeed";
+import "./App.css";
+import { useState } from "react";
+import Navbar from "./navbar";
 
 function App() {
-  const[news, setnews] = useState([]);
-  
-  const fetchNews = async (country, category)  => {
-    try{
-     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=370390adb3da4228aa9fd43e61343d65`);
-     const newsArticles = await response.json();
-     setnews(newsArticles.articles);
-    }catch(error){
+  const [news, setnews] = useState([]);
+
+  const fetchNews = async (country, category) => {
+    try {
+      const response = await fetch(
+        `https://newsdata.io/api/1/news?apikey=pub_6722b45570255c74298799f3efad3fa576ac&country=${country}&category=${category}`
+      );
+      const newsArticles = await response.json();
+      setnews(newsArticles.results);
+    } catch (error) {
       alert("Try again");
     }
-
-  }
+  };
   console.log(news);
 
- 
   return (
     <div>
-      
       <Navbar fetch={fetchNews} />
       {news.map((items) => (
-        <NewsFeed key= {items.title} title= {items.title} description={items.description} link={items.url} image={items.urlToImage} content={items.content}/>
+        <NewsFeed
+          key={items.title}
+          title={items.title}
+          description={items.description}
+          link={items.url}
+          image={items.image_url}
+          content={items.content}
+        />
       ))}
-      
-   
     </div>
-    
   );
 }
 
 export default App;
-
-
-
-
--- src/navbar.js
+++ src/navbar.js
@@ -1,31 +1,43 @@
-import React from "react"; 
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-function Navbar(props){
-    const[currentCountry, setcurrentCountry] = useState("IN");
+function Navbar(props) {
+  const [currentCountry, setcurrentCountry] = useState("IN");
 
-    return(
-        
-
+  return (
     <nav className="navbar">
-    <div className="navbar_logo">
-    <h1 className="logo_title">TIMES OF</h1>
-      <select className="logo_dropdown" onChange={(event) => setcurrentCountry(event.target.value) }>
-      <option value="IN">INDIA</option>
-      <option value="US">US</option>
-      </select>
-      
-    </div>
-    <div className="navbar_category">
-      <input className="nav_content" type="button" value="Business" onClick={() => props.fetch(currentCountry,"business")} />
-      <input className="nav_content" type="button" value="Health" onClick={() => props.fetch(currentCountry,"health")} />
-      <input className="nav_content" type="button" value="General" onClick={() => props.fetch(currentCountry,"general")} />
-    </div>
+      <div className="navbar_logo">
+        <h1 className="logo_title">TIMES OF</h1>
+        <select
+          className="logo_dropdown"
+          onChange={(event) => setcurrentCountry(event.target.value)}
+        >
+          <option value="IN">INDIA</option>
+          <option value="US">US</option>
+        </select>
+      </div>
+      <div className="navbar_category">
+        <input
+          className="nav_content"
+          type="button"
+          value="Business"
+          onClick={() => props.fetch(currentCountry, "business")}
+        />
+        <input
+          className="nav_content"
+          type="button"
+          value="Health"
+          onClick={() => props.fetch(currentCountry, "health")}
+        />
+        <input
+          className="nav_content"
+          type="button"
+          value="entertainment"
+          onClick={() => props.fetch(currentCountry, "entertainment")}
+        />
+      </div>
     </nav>
-
-    );
+  );
 }
-
-
 
 export default Navbar;
